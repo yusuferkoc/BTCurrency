ea4ffd2072d04f3df01c28d4bca116db1fbec826
yusuf <yusuferkoc99@gmail.com>
2023-08-29 14:32:54 +0300

    BTCurrency

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Yusuf Erkoç on 28.08.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
